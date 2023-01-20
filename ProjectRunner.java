535aff3447a73327abdd041870941b6fcd983271
Gowroji Sunil <48122892+sgowroji@users.noreply.github.com>
2023-01-20 12:08:15 +0530

    test PR
    test PR

--- ProjectRunner.java
+++ ProjectRunner.java
@@ -2,6 +2,9 @@ package com.example;
 
 public class ProjectRunner {
     public static void main(String args[]) {
+        // Testing the classs
         Greeting.sayHi();
+        
+        
     }
 }
