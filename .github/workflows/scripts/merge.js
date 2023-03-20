27a20b415ca3514bce8497045fa3585c2bc0d3c6
Gowroji Sunil <48122892+sgowroji@users.noreply.github.com>
2023-03-20 09:25:35 +0530

    Create

---
+++ .github/workflows/scripts/merge.js
@@ -0,0 +1,15 @@
+const prs = ${{ steps.get_prs.outputs.prs }};
+          const now = new Date();
+          const dayInMillis = 1000 * 60 * 60 * 24;
+          for (const pr of prs) {
+            const mergedAt = new Date(pr.merged_at);
+            if (now - mergedAt >= dayInMillis) {
+              const comment = `This pull request was approved and merged more than a day ago. Thank you for your contribution! :smile: :+1:`;
+              await github.issues.createComment({
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                issue_number: pr.number,
+                body: comment
+              });
+            }
+          }
