c0fd06cb39ea18ef0f26a055c458d1ce61a9899a
Gowroji Sunil <48122892+sgowroji@users.noreply.github.com>
2023-03-21 15:35:27 +0530

    Update merge.js

--- .github/workflows/scripts/merge.js
+++ .github/workflows/scripts/merge.js
@@ -1,18 +1,46 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-module.exports = async ({ github, context }) => {
-const prs = ${{ steps.get_prs.outputs.prs }};
-const now = new Date();
-const dayInMillis = 1000 * 60 * 60 * 24;
-for (const pr of prs) {
-  const mergedAt = new Date(pr.merged_at);
-  if (now - mergedAt >= dayInMillis) {
-    const comment = `This pull request was approved and merged more than a day ago. Thank you for your contribution! :smile: :+1:`;
-    await github.issues.createComment({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: pr.number,
-      body: comment
+async function run() {
+  const token = core.getInput('GITHUB_TOKEN');
+  const octokit = github.getOctokit(token);
+
+  const owner = github.context.payload.repository.owner.login;
+  const repo = github.context.payload.repository.name;
+
+  const { data: pullRequests } = await octokit.pulls.list({
+    owner,
+    repo,
+    state: 'open'
+  });
+
+  for (const pullRequest of pullRequests) {
+    const reviews = await octokit.pulls.listReviews({
+      owner,
+      repo,
+      pull_number: pullRequest.number
     });
+
+    const approvedReviews = reviews.data.filter(
+      (review) => review.state === 'APPROVED'
+    );
+
+    if (approvedReviews.length > 0) {
+      const lastApprovedReview = approvedReviews[approvedReviews.length - 1];
+      const now = new Date();
+      const oneDayAgo = new Date(now.setDate(now.getDate() - 1));
+
+      if (new Date(lastApprovedReview.submitted_at) < oneDayAgo) {
+        const message = `Hello @${pullRequest.user.login}, it has been more than 24 hours since this pull request was approved. Can we merge this now? :smile:`;
+        await octokit.issues.createComment({
+          owner,
+          repo,
+          issue_number: pullRequest.number,
+          body: message
+        });
+      }
+    }
   }
 }
-}
+
+run();
